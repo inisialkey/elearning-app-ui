ee921e4a1ffb67a66b38a93bb07889ad37102239
inisialkey <okidorvel404@gmail.com>
2024-11-28 15:06:56 +0700

    add datas, utils, theme, colors

---
+++ lib/datas/user_profile.dart
@@ -0,0 +1,5 @@
+const userProfile = {
+  "image": "assets/images/user_profile.jpg",
+  "full_name": "Robert Williams",
+  "email": "robert.williams@email.com"
+};
---
+++ lib/root_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({super.key});
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
